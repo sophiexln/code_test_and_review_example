3e69c7e434f5f90c28a09a31d525321c41ccd13e
inmojang <inmo3592@gmail.com>
2025-01-04 02:10:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import numpy as np
-
+def add(a,b):
+  c = a + b
+  return c
+  
 def main():
   print("this is main")
-
+  a = 3
+  b = 5
+  c = add(a,b)
+  print("a + b = ", c)
+  
 
 def test():
   print("this is good")
-
-print("Is this okay????")
-print("I think this is now okay")
 
 main()
